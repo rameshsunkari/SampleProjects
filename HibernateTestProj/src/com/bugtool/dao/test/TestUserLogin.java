42c8d7ba4eff6bea0af9b029c38904757e713d60
rsunkari <rameshsunkari@gmail.com>
2013-10-24 15:01:15 +0530

    Sample Project for Hibernate Standalone project

--- HibernateTestProj/src/com/bugtool/dao/test/TestUserLogin.java
+++ HibernateTestProj/src/com/bugtool/dao/test/TestUserLogin.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 
 import com.bugtool.model.UserInfo;
 
@@ -13,10 +15,15 @@ public class TestUserLogin {
 		
 		Session session =  HibernateUtil.getSessionFactory().openSession();
 		Criteria criteria = session.createCriteria(UserInfo.class);
-		criteria.setMaxResults(10);
+		
+		String email = "test.test@test.com";
+		String password = "test";
+		Criterion criterion = Restrictions.and(Restrictions.eq("emailid", email),
+				Restrictions.eq("pswd", password));
+		criteria.add(criterion);
 		List<UserInfo> userList = criteria.list();
 		for (UserInfo user : userList) {
-			System.out.println(user.getEmailid());
+			System.out.println(user.getUserid());
 		}
 	}
 }
